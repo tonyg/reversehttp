63dfc972a07ba4ca7eb3c44ad0891821de0738a6
Tony Garnock-Jones <tonyg@kcbbs.gen.nz>
2009-06-20 09:21:07 +0100

    Remove obsolete messaging experiments

--- priv/www/pub.js
+++
@@ -1,9 +0,0 @@
-function sendMessage() {
-    new Messaging.Message(null, $("targetName").value, $("messageBody").value, "text/plain")
-    .deliver(makeLoggingDeliverOptions("send"));
-}
-
-function pub_main() {
-    $("targetName").value =
-	$("targetName").value.replace("???", new Url(window.location).getHostPort());
-}
